108408dd1e3ebff33e0717403879dc73cc0845a8
Johannes Poehn <johannes.poehn@gmail.com>
2020-09-24 23:21:04 +0200

    Introduce ItemTypes, Create sln

--- csharpcore/GildedRose.cs
+++ csharpcore/GildedRose.cs
@@ -12,76 +12,24 @@ namespace csharpcore
 
         public void UpdateQuality()
         {
-            for (var i = 0; i < Items.Count; i++)
+            foreach (var item in Items)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if(!(item is ShopItem shopItem))
                 {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
+                    continue;
                 }
-                else
+                shopItem.DecreaseSellIn();
+                switch (shopItem)
                 {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
+                    case DegradingItem degradingItem:
+                        degradingItem.Degrade();
+                        break;
+                    case AgedItem agedItem:
+                        agedItem.AgeFurther();
+                        break;
+                    case ConcertTicket concertTicket:
+                        concertTicket.Aggrade();
+                        break;
                 }
             }
         }
--- csharpcore/Item.cs
+++ csharpcore/Item.cs
@@ -1,9 +1,94 @@
-﻿namespace csharpcore
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace csharpcore
 {
     public class Item
     {
         public string Name { get; set; }
         public int SellIn { get; set; }
         public int Quality { get; set; }
+    }
+
+    public abstract class ShopItem : Item
+    {
+        protected static readonly int MaxQuality = 50;
+
+        public void DecreaseSellIn()
+        {
+            // prevent overflow in 5.9m years!
+            SellIn = Math.Max(-1, SellIn - 1);
+        }
+    }
+
+    public class DegradingItem : ShopItem
+    {
+        protected virtual int GetDegration()
+        {
+            if (SellIn < 0)
+            {
+                return 2;
+            }
+
+            return 1;
+        }
+
+        public void Degrade()
+        {
+            Quality = Math.Max(0, Quality - GetDegration());
+        }
+    }
+
+    public class AgedItem : ShopItem
+    {
+        public void AgeFurther()
+        {
+            Quality = Math.Min(MaxQuality, Quality + 1);
+        }
+    }
+
+    public class EpicItem : Item
+    {
+        public void BeholdMyEpicness() {}
+    }
+
+    public class ConcertTicket : ShopItem
+    {
+        private static readonly int CloseSellIn = 11;
+        private static readonly int VeryCloseSellIn = 6;
+
+        private int GetAggration()
+        {
+            if (SellIn < VeryCloseSellIn)
+            {
+                return 3;
+            }
+
+            if (SellIn < CloseSellIn)
+            {
+                return 2;
+            }
+
+            return 1;
+        }
+
+        public void Aggrade()
+        {
+            if (SellIn < 0)
+            {
+                Quality = 0;
+                return;
+            }
+
+            Quality = Math.Min(MaxQuality, Quality + GetAggration());
+        }
+    }
+
+    public class ConjuredItem : DegradingItem
+    {
+        protected override int GetDegration()
+        {
+            return base.GetDegration()*2;
+        }
     }
 }
